5e6ff6cd4c0e51d954dc2a13da19e2ce30d7efd6
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-11-20 12:03:10 -0500

    commit tree 5701ba8f663beacd45be45a255dcc59e7b1b851e

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,43 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        
+    end
+
+    def self.save(name, type, db)
+
+        # db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name , type)
+
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+        SQL
+
+        # poke = db.execute(sql, id).flatten ==> Same thing with db.execute(sql, id)[0] to get data out of 
+        poke = db.execute(sql, id)[0]
+        Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db:db)
+    end
+
+    # def self.find(id, db)
+    #     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+    #     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    # end
+
 end
